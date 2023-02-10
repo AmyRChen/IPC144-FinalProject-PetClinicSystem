659d8b92b7ce5960b9684e4500d6cb217afd60c3
Ping-Ju Chen <103694860+AmyRChen@users.noreply.github.com>
2023-02-09 23:18:30 -0500

    Update clinic.h

--- MS2/clinic.h
+++ MS2/clinic.h
@@ -1,18 +1,3 @@
-/*
-*****************************************************************************
-                        Assignment 1 - Milestone 2
-Full Name  : Ping-Ju, Chen
-Student ID#: 151043205
-Email      : pchen72@myseneca.ca
-Section    : NAA
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-*****************************************************************************
-*/
-
 // SAFE-GUARD: 
 // It is good practice to apply safe-guards to header files
 // Safe-guard's ensures only 1 copy of the header file is used in the project build
